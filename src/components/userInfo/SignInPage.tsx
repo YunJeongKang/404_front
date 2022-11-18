28ce9e6fa1ab47ed3026e8d2e62b7c988d214b0e
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-11-18 19:37:46 +0900

    feat(axios): add to code axios

--- src/components/userInfo/SignInPage.tsx
+++ src/components/userInfo/SignInPage.tsx
@@ -1,6 +1,8 @@
 import UserInfoForm from "@styles/indexForm";
 import { TextInputTemplate } from "@utils/common/props/userInfo/TotalFormTemplate";
+import axios from "axios";
 import { useState } from "react";
+import PATH from "@utils/routes/PATH";
 
 const SignInPage = () => {
   const [values, setValues] = useState<any>([
@@ -17,6 +19,14 @@ const SignInPage = () => {
       [name]: value,
     });
   };
+
+  const { URL, SIGNIN } = PATH;
+
+  function onSubmit() {
+    axios.post(`${URL}${SIGNIN}`, { ...values });
+    console.log({ ...values });
+  }
+
   return (
     /* query: string;
       pattern: string;
@@ -26,31 +36,32 @@ const SignInPage = () => {
       value?: string; 
       placeholder?: string; */
 
-    <UserInfoForm className="py-4 px-12 gap-2">
+    <UserInfoForm className="py-4 px-12 gap-2" onSubmit={onSubmit}>
       <TextInputTemplate
         onChange={onChange}
         value={values.nickName}
         query="닉네임"
-        pattern="[ㄱ-ㅎ가-힣A-Za-z0-9]+$"
-        name="닉네임"
+        pattern="[ㄱ-ㅎ가-힣A-Za-z0-9]{3,8}$"
+        name="nickname"
         dangerText="3~8자의 닉네임을 입력하세요"
         placeholder="닉네임을 입력하세요"
       />
       <TextInputTemplate
         onChange={onChange}
-        value={values.email}
+        value={values.email || ""}
         query="이메일 주소"
         pattern="([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"
-        name="성별"
+        name="email"
         dangerText="올바른 이메일 형식을 입력하세요"
         placeholder="이메일을 입력하세요"
       />
       <TextInputTemplate
         onChange={onChange}
-        value={values.password}
+        value={values.password || ""}
         query="비밀번호"
         pattern="(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$"
-        name="성별"
+        name="password"
+        type="password"
         dangerText="숫자, 영문, 특수문자를 각 1개 이상 포함한 8자리 이상의 비밀번호를 입력하세요"
         placeholder="비밀번호를 입력하세요"
       />

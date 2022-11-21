36b30ccececd84a9b6cf7a70e95fa5f3d6b8af64
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-11-22 01:21:22 +0900

    feat(login): save to AuthState value on new logic

--- src/utils/common/props/userInfo/TotalFormTemplate.tsx
+++ src/utils/common/props/userInfo/TotalFormTemplate.tsx
@@ -48,3 +48,42 @@ export const TextInputTemplate = ({
     </fieldset>
   );
 };
+
+export const CheckPWTemplate = ({
+  query,
+  value,
+  pattern,
+  name,
+  dangerText = "This is not correct",
+  type,
+  placeholder,
+  onChange,
+}: TextInputTemplateInterface) => {
+  return (
+    <fieldset className="flex gap-2 p-[3px]">
+      <span>{query} :</span>
+      <div className="flex flex-col items-start gap-1">
+        <input
+          className="border peer dark:text-dark px-1"
+          type={type}
+          value={value}
+          pattern={`^${pattern}`}
+          name={name}
+          placeholder={placeholder}
+          required
+          onChange={onChange}
+          autoComplete="true"
+        />
+        <span className="hidden peer-invalid:block">
+          <span className="text-danger text-sm">
+            {value === "" ? (
+              <></>
+            ) : (
+              <span className="text-danger text-sm">{dangerText}</span>
+            )}
+          </span>
+        </span>
+      </div>
+    </fieldset>
+  );
+};

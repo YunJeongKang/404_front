2695930739ab07272680c9b5e1b217f82bbec872
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-11-21 13:54:38 +0900

    feat(interface): create  userProfile state data type interface

--- src/models/user/UserDetail.ts
+++ src/models/user/UserDetail.ts
@@ -1,1 +1,28 @@
-export default interface UserDetail {}
+export interface mainInfoInterface {
+  gender: string;
+  birth: string;
+  married: string;
+  sisBro: string;
+  region: string;
+}
+
+export interface regionInfoInterface {
+  kangwon: string;
+  gyungki: string;
+  gyungnam: string;
+  gyungbuk: string;
+  gwangju: string;
+  daegu: string;
+  daejeon: string;
+  busan: string;
+  seoul: string;
+  sejong: string;
+  ulsan: string;
+  incheon: string;
+  jeonnam: string;
+  jeonbuk: string;
+  jeju: string;
+  chungnam: string;
+  chungbuk: string;
+  foreign: string;
+}

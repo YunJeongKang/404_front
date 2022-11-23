2baeefee5f4e25d5f16b847666cdb1e2f47b66c5
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-11-23 10:30:50 +0900

    feat(data): update client data type

--- src/data/main_info/salary.ts
+++ src/data/main_info/salary.ts
@@ -1,25 +1,22 @@
 import { RegionSelectType } from "@models/CPType";
 
 export const salaryList: RegionSelectType[] = [
-  { regionInfoName: "강원도", value: "a" },
-  { regionInfoName: "경기도", value: "b" },
-  { regionInfoName: "경상남도", value: "c" },
-  { regionInfoName: "경상북도", value: "d" },
-  { regionInfoName: "광주", value: "e" },
-  { regionInfoName: "대구", value: "f" },
-  { regionInfoName: "대전", value: "g" },
-  { regionInfoName: "부산", value: "h" },
-  { regionInfoName: "서울", value: "i" },
-  { regionInfoName: "세종", value: "z" },
-  { regionInfoName: "울산", value: "j" },
-  { regionInfoName: "인천", value: "k" },
-  { regionInfoName: "전라남도", value: "l" },
-  { regionInfoName: "전라북도", value: "n" },
-  { regionInfoName: "제주도", value: "m" },
-  { regionInfoName: "충청남도", value: "o" },
-  { regionInfoName: "충청북도", value: "p" },
-  { regionInfoName: "해외", value: "q" },
+  { regionInfoName: "적음", value: "1" },
+  { regionInfoName: "2천미만", value: "12" },
+  { regionInfoName: "2천대", value: "2" },
+  { regionInfoName: "3천대", value: "3" },
+  { regionInfoName: "4천대", value: "4" },
+  { regionInfoName: "5천대", value: "5" },
+  { regionInfoName: "6천대", value: "6" },
+  { regionInfoName: "7천대", value: "7" },
+  { regionInfoName: "8천대", value: "8" },
+  { regionInfoName: "9천대", value: "9" },
+  { regionInfoName: "1-3억", value: "10" },
+  { regionInfoName: "3-5억", value: "13" },
+  { regionInfoName: "5-10억", value: "14" },
+  { regionInfoName: "10-20억", value: "15" },
+  { regionInfoName: "20-30억", value: "18" },
+  { regionInfoName: "30억", value: "16" },
 ];
 
-// 내용을 한 단계 depth 동결 시키는 것 -> 불변성 조금 보장 -> value 바뀔 수 있음.
 Object.freeze(salaryList);
--- src/data/main_info/vehicle.ts
+++ src/data/main_info/vehicle.ts
@@ -1,15 +1,14 @@
 import { RadioType } from "@models/CPType";
 
 export const vehicleList: RadioType[] = [
-  { labelName: "소형", htmlFor: "", value: "1" },
-  { labelName: "준중형", htmlFor: "", value: "2" },
-  { labelName: "중형", htmlFor: "", value: "3" },
-  { labelName: "대형", htmlFor: "", value: "4" },
-  { labelName: "수입", htmlFor: "", value: "5" },
-  { labelName: "SUV", htmlFor: "", value: "6" },
-  { labelName: "승합", htmlFor: "", value: "7" },
-  { labelName: "", htmlFor: "", value: "8" },
-  { labelName: "없음", htmlFor: "", value: "9" },
+  { labelName: "소형", htmlFor: "sm", value: "1" },
+  { labelName: "준중형", htmlFor: "sm-md", value: "2" },
+  { labelName: "중형", htmlFor: "md", value: "3" },
+  { labelName: "대형", htmlFor: "lg", value: "4" },
+  { labelName: "수입", htmlFor: "foreign", value: "5" },
+  { labelName: "SUV", htmlFor: "suv", value: "6" },
+  { labelName: "승합", htmlFor: "van", value: "7" },
+  { labelName: "없음", htmlFor: "none", value: "9" },
   { labelName: "기타", htmlFor: "other", value: "0" },
 ];
 Object.freeze(vehicleList);

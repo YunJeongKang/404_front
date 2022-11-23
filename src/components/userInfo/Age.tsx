ed06993a18e3d071c2211ef6d0c6412c1dd3ca73
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-11-23 09:49:57 +0900

    feat(style): fix to UserInfoPage UI design

--- src/components/userInfo/Age.tsx
+++ src/components/userInfo/Age.tsx
@@ -13,7 +13,7 @@ const Age = ({ onChange, value }: AgeInterface) => {
       name="birth"
       min="1953-01-01"
       max="2003-12-31"
-      className="w-1/2 border shadow rounded-md px-2 py-1 my-2"
+      className="w-2/3 border shadow rounded-md px-2 py-1 my-2"
       onChange={onChange}
       value={value}
       required

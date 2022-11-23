068453439bd5705e6c287a3279da946b2f9e9e4c
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-11-23 17:19:55 +0900

    feat(logOut): add to logout Btn in protectedPage

--- src/components/home/Mainpage.tsx
+++ src/components/home/Mainpage.tsx
@@ -1,5 +1,18 @@
+import useAuth from "@store/useAuth";
+
 function MainPage() {
-  return <div>홈-인증</div>;
+  const auth = useAuth();
+  return (
+    <div className="flex flex-col flex-wrap">
+      홈-인증
+      <button
+        className="bg-indigo-400 rounded-md shadow"
+        onClick={() => auth.logout()}
+      >
+        로그아웃
+      </button>
+    </div>
+  );
 }
 
 export default MainPage;
--- src/components/routes/ProtectedRoutes.tsx
+++ src/components/routes/ProtectedRoutes.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes, Link } from "react-router-dom";
 import PATH from "@utils/routes/PATH";
 import useAuth from "@store/useAuth";
+import MainPage from "@components/home/Mainpage";
 
 const { HOME, TEST, UI } = PATH;
 
@@ -10,7 +11,7 @@ const ProtectedRoutes = () => {
 
   return (
     <Routes>
-      <Route path={HOME} element={<div>홈</div>} />
+      <Route path={HOME} element={<MainPage />} />
       <Route path={TEST} element={<div>테스트</div>} />
       <Route path={UI} element={<div>UI</div>} />
       <Route path="*" element={<Navigate replace to={HOME} />} />

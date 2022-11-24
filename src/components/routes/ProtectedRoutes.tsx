8aeaf73eb01e751479fab80c1bf6e31686813e1a
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-11-24 19:03:55 +0900

    feat(routes): update routes setting

--- src/components/routes/ProtectedRoutes.tsx
+++ src/components/routes/ProtectedRoutes.tsx
@@ -2,8 +2,12 @@ import { Navigate, Route, Routes, Link } from "react-router-dom";
 import PATH from "@utils/routes/PATH";
 import useAuth from "@store/useAuth";
 import MainPage from "@components/home/Mainpage";
+import RecommendPage from "@components/home/RecommendPage";
+import LikePage from "@components/home/LikePage";
+import ChatPage from "@components/home/ChatPage";
+import UserSettingPage from "@components/home/UserSettingPage";
 
-const { HOME, TEST, UI } = PATH;
+const { HOME, LIKE, RECOMMEND, CHAT, USER } = PATH;
 
 const ProtectedRoutes = () => {
   const auth = useAuth();
@@ -12,8 +16,10 @@ const ProtectedRoutes = () => {
   return (
     <Routes>
       <Route path={HOME} element={<MainPage />} />
-      <Route path={TEST} element={<div>테스트</div>} />
-      <Route path={UI} element={<div>UI</div>} />
+      <Route path={LIKE} element={<LikePage />} />
+      <Route path={RECOMMEND} element={<RecommendPage />} />
+      <Route path={CHAT} element={<ChatPage />} />
+      <Route path={USER} element={<UserSettingPage />} />
       <Route path="*" element={<Navigate replace to={HOME} />} />
     </Routes>
   );

edf448d4ade91fd435d61800702a49130b6da879
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-11-24 16:47:19 +0900

    feat(app): update to app.tsx nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { FiHome } from "react-icons/fi";
+import { CgHeart } from "react-icons/cg";
+import { GiLovers } from "react-icons/gi";
+import { RiChat1Line } from "react-icons/ri";
+import { AiOutlineUser } from "react-icons/ai";
 import PATH from "@utils/routes/PATH";
 import ProtectedRoutes from "@components/routes/ProtectedRoutes";
 import UnauthedRoutes from "@components/routes/UnauthedRoutes";
@@ -8,7 +13,7 @@ import useAuth from "@store/useAuth";
 function App() {
   const auth = useAuth();
   const location = useLocation();
-  const { HOME, TEST, UI, LOGIN } = PATH;
+  const { HOME, LIKE, RECOMMEND, CHAT, USER } = PATH;
   const [currentRoutes, setCurrentRoutes] = useState<React.ReactNode | null>(
     null
   );
@@ -29,16 +34,30 @@ function App() {
   return (
     <div className="App flex flex-row justify-center overflow-x-hidden scrollbar-hide">
       <div className="h-screen max-w-[28rem] z-10 scrollbar-hide">
-        <header>
-          <nav className="flex flex-row justify-center gap-4 py-2">
-            <Link to={HOME}>홈</Link>
-            <Link to={TEST}>테스트</Link>
-            <Link to={UI}>UI</Link>
-          </nav>
-        </header>
-        <main className="flex flex-col items-center w-[28rem] border shadow-md rounded-lg">
+        <main className="flex flex-col items-center w-[28rem] border shadow-inner drop-shadow">
           {currentRoutes}
         </main>
+        {/* {auth.isAuthenticated === true ? ( */}
+        <footer>
+          <nav className="flex flex-row justify-center gap-16 border shadow-md py-2">
+            <Link to={HOME}>
+              <FiHome size="30" />
+            </Link>
+            <Link to={LIKE}>
+              <CgHeart size="30" />
+            </Link>
+            <Link to={RECOMMEND}>
+              <GiLovers size="30" />
+            </Link>
+            <Link to={CHAT}>
+              <RiChat1Line size="30" />
+            </Link>
+            <Link to={USER}>
+              <AiOutlineUser size="30" />
+            </Link>
+          </nav>
+        </footer>
+        {/* ) : null} */}
       </div>
     </div>
   );

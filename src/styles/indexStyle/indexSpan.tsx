1c9541674bbd86b282fb02a0c0b2d77084ef211f
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-11-24 16:51:02 +0900

    chore: rm useless things

--- src/styles/indexStyle/indexSpan.tsx
+++ src/styles/indexStyle/indexSpan.tsx
@@ -4,7 +4,7 @@ import React, { FunctionComponent as FC } from "react";
 const RequiredMark: FC<CommonSpanProps> = (props) => {
   const { children, className, ...restProps } = props;
   return (
-    <span {...restProps} className={`text-red-700 ${className}`} text-lg>
+    <span {...restProps} className={`text-red-700 text-lg ${className}`}>
       *{children}
     </span>
   );

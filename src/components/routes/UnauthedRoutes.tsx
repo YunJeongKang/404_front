85c4fb4f3ac97c9d88f703b0605f0a5a18f27b15
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-11-26 14:57:35 +0900

    feat(UnauthedRoute): apply useEffect

--- src/components/routes/UnauthedRoutes.tsx
+++ src/components/routes/UnauthedRoutes.tsx
@@ -5,20 +5,35 @@ import EasyStartPage from "@components/auth/EasyStartPage";
 import SignUnPage from "@components/auth/SignUpPage";
 import UserInfoPage from "@components/userInfo/UserInfoPage";
 import useAuth from "./../../store/useAuth";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const { LOGIN, EASY_AUTH, SIGNUP, INPUT } = PATH;
 
 function UnauthedRoutes() {
   const auth = useAuth();
+  // 사용자 정보 입력에 따른 삼항연상자
   const autoInput = auth.getReady() === "true";
+  const [currentRoute, setCurrentRoute] = useState<React.ReactNode | null>(
+    null
+  );
+  const [currentPath, setCurrentPath] = useState<string>("");
+
+  useEffect(() => {
+    // 컴포넌트 렌더링 -> 회원가입창 또는 정보입력창
+    const currentRoute = autoInput ? <UserInfoPage /> : <SignUnPage />;
+    setCurrentRoute(currentRoute);
+
+    //경로 렌더링 -> 회원가입창 또는 정보입력창
+    const currentPath = autoInput ? INPUT : SIGNUP;
+    setCurrentPath(currentPath);
+  }, [autoInput, INPUT, SIGNUP]);
+
   return (
     <Routes>
       <Route path={EASY_AUTH} element={<EasyStartPage />} />
       <Route path={LOGIN} element={<LoginPage />} />
-      <Route
-        path={autoInput ? INPUT : SIGNUP}
-        element={autoInput ? <UserInfoPage /> : <SignUnPage />}
-      />
+      <Route path={currentPath} element={currentRoute} />
       <Route path={INPUT} element={<UserInfoPage />} />
       <Route
         path="*"

4f175ea7f99adcaee9ecb7ddb6d19ceee966213c
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-11-26 14:57:43 +0900

    feat(Footer,Header): separate to component in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,20 @@
 import { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
-import { AiFillHome, AiOutlineHome } from "react-icons/ai";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
-import {
-  HiBadgeCheck,
-  HiOutlineBadgeCheck,
-  HiUser,
-  HiOutlineUser,
-} from "react-icons/hi";
-import { RiChat1Line, RiChat1Fill } from "react-icons/ri";
-import PATH from "@utils/routes/PATH";
+import { useLocation } from "react-router-dom";
 import ProtectedRoutes from "@components/routes/ProtectedRoutes";
 import UnauthedRoutes from "@components/routes/UnauthedRoutes";
 import useAuth from "@store/useAuth";
+import Footer from "@utils/common/app/Footer";
+import Header from "@utils/common/app/Header";
 
 function App() {
   const auth = useAuth();
   const location = useLocation();
-  const { HOME, LIKE, RECOMMEND, CHAT, USER } = PATH;
   const [currentRoutes, setCurrentRoutes] = useState<React.ReactNode | null>(
     null
   );
 
+  const authPageUI = "min-h-[38rem] max-h-[38rem]";
+  const unAuthPageUI = "min-h-[43rem] max-h-[43rem]";
   useEffect(() => {
     console.log("추적 가능한 경로", location.pathname);
   }, [location.pathname]);
@@ -35,57 +28,16 @@ function App() {
 
   return (
     <div className="App flex flex-row justify-center overflow-x-hidden scrollbar-hide">
-      <div className="h-screen max-w-[28rem] z-10 scrollbar-hide">
+      <div className="h-screen max-w-[28rem] z-10 scrollbar-hide py-2">
+        {isTrue ? <Header /> : null}
         <main
-          className={`flex flex-col items-center w-[26rem] border shadow-inner drop-shadow ${
-            isTrue
-              ? "min-h-[41.6rem] max-h-[41.6rem]"
-              : "min-h-[44.6rem] max-h-[44.6rem]"
-          } overflow-hidden overflow-y-auto scrollbar-hide justify-center bg-sky-100`}
+          className={`flex flex-col items-center w-[26rem] border-x shadow-sm drop-shadow ${
+            isTrue ? authPageUI : unAuthPageUI
+          } overflow-hidden overflow-y-auto scrollbar-hide justify-center border`}
         >
           {currentRoutes}
         </main>
-        {isTrue ? (
-          <footer>
-            <nav className="flex flex-row justify-center gap-14 border shadow-md py-2 ">
-              <Link to={HOME}>
-                {location.pathname === HOME ? (
-                  <AiFillHome size="30" />
-                ) : (
-                  <AiOutlineHome size="30" />
-                )}
-              </Link>
-              <Link to={LIKE}>
-                {location.pathname === LIKE ? (
-                  <FaHeart size="30" />
-                ) : (
-                  <FaRegHeart size="30" />
-                )}
-              </Link>
-              <Link to={RECOMMEND}>
-                {location.pathname === RECOMMEND ? (
-                  <HiBadgeCheck size="30" />
-                ) : (
-                  <HiOutlineBadgeCheck size="30" />
-                )}
-              </Link>
-              <Link to={CHAT}>
-                {location.pathname === CHAT ? (
-                  <RiChat1Fill size="30" />
-                ) : (
-                  <RiChat1Line size="30" />
-                )}
-              </Link>
-              <Link to={USER}>
-                {location.pathname === USER ? (
-                  <HiUser size="30" />
-                ) : (
-                  <HiOutlineUser size="30" />
-                )}
-              </Link>
-            </nav>
-          </footer>
-        ) : null}
+        {isTrue ? <Footer /> : null}
       </div>
     </div>
   );

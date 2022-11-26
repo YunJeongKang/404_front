8894d308ea6390c29cdb49051e520f2f5d07b01f
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-11-26 14:57:14 +0900

    feat(Footer,Header): separate to component in app.tsx

---
+++ src/utils/common/app/Footer.tsx
@@ -0,0 +1,61 @@
+import { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { AiFillHome, AiOutlineHome } from "react-icons/ai";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { RiChat1Line, RiChat1Fill } from "react-icons/ri";
+import {
+  HiBadgeCheck,
+  HiOutlineBadgeCheck,
+  HiUser,
+  HiOutlineUser,
+} from "react-icons/hi";
+import PATH from "@utils/routes/PATH";
+
+const Footer = () => {
+  const { HOME, LIKE, RECOMMEND, CHAT, USER } = PATH;
+  const location = useLocation();
+
+  return (
+    <footer>
+      <nav className="flex flex-row justify-center gap-14 border shadow-md py-2 ">
+        <Link to={HOME}>
+          {location.pathname === HOME ? (
+            <AiFillHome size="30" />
+          ) : (
+            <AiOutlineHome size="30" />
+          )}
+        </Link>
+        <Link to={LIKE}>
+          {location.pathname === LIKE ? (
+            <FaHeart size="30" />
+          ) : (
+            <FaRegHeart size="30" />
+          )}
+        </Link>
+        <Link to={RECOMMEND}>
+          {location.pathname === RECOMMEND ? (
+            <HiBadgeCheck size="30" />
+          ) : (
+            <HiOutlineBadgeCheck size="30" />
+          )}
+        </Link>
+        <Link to={CHAT}>
+          {location.pathname === CHAT ? (
+            <RiChat1Fill size="30" />
+          ) : (
+            <RiChat1Line size="30" />
+          )}
+        </Link>
+        <Link to={USER}>
+          {location.pathname === USER ? (
+            <HiUser size="30" />
+          ) : (
+            <HiOutlineUser size="30" />
+          )}
+        </Link>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/utils/common/app/Header.tsx
+++ src/utils/common/app/Header.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { BsBellFill } from "react-icons/bs";
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header className="flex justify-between bg-blue-600 shadow-sm border py-1">
+      <span
+        className="text-2xl px-3 text-center font-bold text-white"
+        style={{ fontFamily: "ELAND_Choice_M" }}
+      >
+        After Like
+      </span>
+      <BsBellFill size="24" color="white" className="m-1 mx-2" />
+    </header>
+  );
 };
 
 export default Header;

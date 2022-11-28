225f91934974a44d8b6cc4e9335fdd24e9cff957
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-11-28 16:22:57 +0900

    chore : rm unnecessary things

--- src/styles/indexStyle/indexSpan.tsx
+++ src/styles/indexStyle/indexSpan.tsx
@@ -2,17 +2,6 @@ import { CommonSpanProps } from "@utils/common/props";
 import React, { FunctionComponent as FC } from "react";
 import { MdOutlineArrowForwardIos } from "react-icons/md";
 
-const RequiredMark: FC<CommonSpanProps> = (props) => {
-  const { children, className, ...restProps } = props;
-  return (
-    <span {...restProps} className={`text-red-700 text-lg  ${className}`}>
-      *{children}
-    </span>
-  );
-};
-
-export default RequiredMark;
-
 export const UserSettingSpan: FC<CommonSpanProps> = (props) => {
   const { children, className, ...restProps } = props;
   return (

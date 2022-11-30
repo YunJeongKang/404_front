8b8e2c2116ae2b00aabaddee409c91f58852171b
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-11-30 15:11:48 +0900

    axios(recommendPAge): add to email logic

--- src/components/home/RecommendPage.tsx
+++ src/components/home/RecommendPage.tsx
@@ -16,7 +16,7 @@ const RecommendPage = () => {
         className="text-4xl text-center w-full h-[14%] font-bold font-sebang drop-shadow-[0.08em_0.08em_0_rgba(255_155_0_/_0.8)]
       active:drop-shadow-[0.08em_0.08em_0_rgba(105_127_255_/_0.8)] active:scale-95 duration-100"
         onClick={() => {
-          setClick(true);
+          !isClick && setClick(true);
           const userEmail = client.getUserEmail();
           axios.post(`${URL}${RECOMMEND}`, {
             isClick: isClick,
@@ -27,66 +27,71 @@ const RecommendPage = () => {
       >
         천생연분
       </span>
+      {!isClick && (
+        <span className="absolute text-xs top-12">위 글씨를 클릭하세요!</span>
+      )}
       {/* 추천 1, 2, 3등 큰 박스 */}
-      <div className="relative flex flex-col items-start w-full h-5/6">
-        {/* 유저 정보 박스 */}
-        <RecommendTemplate
-          ratingDiv={
-            <>
-              <FaCrown
-                className="absolute top-[-45px] left-[-20px] z-20 rotate-[340deg]"
-                size="28"
-                color="yellow"
-              />
-              <div className=" absolute z-20 left-[-15px] top-[-30px] flex justify-center h-1/4 w-[13.5%] text-center">
-                <p className="relative text-center py-1.5 font-bold">1위</p>
-              </div>
-            </>
-          }
-          img="/yena.jpg"
-          job="직업"
-          region="지역"
-          username="닉네임"
-        />
-        <RecommendTemplate
-          ratingDiv={
-            <>
-              <div className="absolute z-20 right-[-14px] top-[-27px] flex justify-center rounded-full h-1/4 w-[13.5%] text-center">
-                <p className="relative text-center py-1.5 font-bold">2위</p>
-              </div>
-              <FaCrown
-                className="absolute top-[-40px] right-[-22px] z-20 rotate-[20deg]"
-                size="28"
-                color="silver"
-              />
-            </>
-          }
-          img="/Hyewoon.jpg"
-          job="직업"
-          region="지역"
-          username="닉네임"
-          className="!ml-14"
-        />
-        <RecommendTemplate
-          ratingDiv={
-            <>
-              <div className="absolute z-20 right-0 top-[-30px] flex justify-center rounded-full h-1/4 w-[13.5%] text-center">
-                <p className="relative text-center py-1.5 font-bold">3위</p>
-              </div>
-              <FaCrown
-                className="absolute top-[-45px] right-[-8px] z-20 rotate-[20deg]"
-                size="28"
-                color="#bf360c"
-              />
-            </>
-          }
-          img="/Jien.jpg"
-          job="직업"
-          region="지역"
-          username="닉네임"
-          className="!ml-24"
-        />
-      </div>
+      {isClick && (
+        <div className="relative flex flex-col items-start w-full h-5/6">
+          {/* 유저 정보 박스 */}
+          <RecommendTemplate
+            ratingDiv={
+              <>
+                <FaCrown
+                  className="absolute top-[-45px] left-[-20px] z-20 rotate-[340deg]"
+                  size="28"
+                  color="yellow"
+                />
+                <div className=" absolute z-20 left-[-15px] top-[-30px] flex justify-center h-1/4 w-[13.5%] text-center">
+                  <p className="relative text-center py-1.5 font-bold">1위</p>
+                </div>
+              </>
+            }
+            img="/yena.jpg"
+            job="가수"
+            region="서울어딘가"
+            username="최예나"
+          />
+          <RecommendTemplate
+            ratingDiv={
+              <>
+                <div className="absolute z-20 right-[-14px] top-[-27px] flex justify-center rounded-full h-1/4 w-[13.5%] text-center">
+                  <p className="relative text-center py-1.5 font-bold">2위</p>
+                </div>
+                <FaCrown
+                  className="absolute top-[-40px] right-[-22px] z-20 rotate-[20deg]"
+                  size="28"
+                  color="silver"
+                />
+              </>
+            }
+            img="/Hyewoon.jpg"
+            job="배우"
+            region="모름"
+            username="김혜인?"
+            className="!ml-14"
+          />
+          <RecommendTemplate
+            ratingDiv={
+              <>
+                <div className="absolute z-20 right-0 top-[-30px] flex justify-center rounded-full h-1/4 w-[13.5%] text-center">
+                  <p className="relative text-center py-1.5 font-bold">3위</p>
+                </div>
+                <FaCrown
+                  className="absolute top-[-45px] right-[-8px] z-20 rotate-[20deg]"
+                  size="28"
+                  color="#bf360c"
+                />
+              </>
+            }
+            img="/Jien.jpg"
+            job="배우"
+            region="천원짜리"
+            username="변호사"
+            className="!ml-24"
+          />
+        </div>
+      )}
     </div>
   );
 };

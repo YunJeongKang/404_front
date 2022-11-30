62d3f555d41d66d780e33e8008c9af4ba098dca0
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-11-30 18:01:03 +0900

    rm(mainPage): rm logoutBTN

--- src/components/home/Mainpage.tsx
+++ src/components/home/Mainpage.tsx
@@ -2,17 +2,7 @@ import useAuth from "@store/useAuth";
 
 function MainPage() {
   const auth = useAuth();
-  return (
-    <div className="flex flex-col flex-wrap">
-      메인 페이지
-      <button
-        className="bg-indigo-400 rounded-md shadow"
-        onClick={() => ((auth.isAuthenticated = false), auth.logout())}
-      >
-        로그아웃
-      </button>
-    </div>
-  );
+  return <div className="flex flex-col flex-wrap">메인 페이지</div>;
 }
 
 export default MainPage;

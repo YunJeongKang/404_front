e470cfd060618c8ec4b8df4313b0e764905ccb4e
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-11-30 13:39:58 +0900

    style(recommended): create new UI for reco

--- src/components/home/RecommendPage.tsx
+++ src/components/home/RecommendPage.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import RecommendTemplate from "@utils/common/app/RecommendTemplate";
 import axios from "axios";
 import PATH from "@utils/routes/PATH";
+import { FaCrown } from "react-icons/fa";
 
 const RecommendPage = () => {
   const [isClick, setClick] = useState<boolean>(false);
@@ -24,9 +25,16 @@ const RecommendPage = () => {
         {/* 유저 정보 박스 */}
         <RecommendTemplate
           ratingDiv={
-            <div className="absolute z-20 left-[-15px] top-[-20px] flex justify-center border-2 rounded-full h-1/4 w-[13.5%] text-center">
-              <p className="relative text-center py-1.5 font-bold">1위</p>
-            </div>
+            <>
+              <FaCrown
+                className="absolute top-[-45px] left-[-20px] z-20 rotate-[340deg]"
+                size="28"
+                color="yellow"
+              />
+              <div className=" absolute z-20 left-[-15px] top-[-30px] flex justify-center h-1/4 w-[13.5%] text-center">
+                <p className="relative text-center py-1.5 font-bold">1위</p>
+              </div>
+            </>
           }
           img="/yena.jpg"
           job="직업"
@@ -35,21 +43,35 @@ const RecommendPage = () => {
         />
         <RecommendTemplate
           ratingDiv={
-            <div className="absolute z-20 right-[-14px] top-[-17px] flex justify-center border-2 rounded-full h-1/4 w-[13.5%] text-center">
-              <p className="relative text-center py-1.5 font-bold">2위</p>
-            </div>
+            <>
+              <div className="absolute z-20 right-[-14px] top-[-27px] flex justify-center rounded-full h-1/4 w-[13.5%] text-center">
+                <p className="relative text-center py-1.5 font-bold">2위</p>
+              </div>
+              <FaCrown
+                className="absolute top-[-40px] right-[-22px] z-20 rotate-[20deg]"
+                size="28"
+                color="silver"
+              />
+            </>
           }
           img="/Hyewoon.jpg"
           job="직업"
           region="지역"
           username="닉네임"
-          className="ml-14"
+          className="ml-14 "
         />
         <RecommendTemplate
           ratingDiv={
-            <div className="absolute z-20 right-[-9px] top-[-20px] flex justify-center border-2 rounded-full h-1/4 w-[13.5%] text-center">
-              <p className="relative text-center py-1.5 font-bold">3위</p>
-            </div>
+            <>
+              <div className="absolute z-20 right-0 top-[-30px] flex justify-center rounded-full h-1/4 w-[13.5%] text-center">
+                <p className="relative text-center py-1.5 font-bold">3위</p>
+              </div>
+              <FaCrown
+                className="absolute top-[-45px] right-[-8px] z-20 rotate-[20deg]"
+                size="28"
+                color="#bf360c"
+              />
+            </>
           }
           img="/Jien.jpg"
           job="직업"

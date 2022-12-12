afda50c25fff2754bc4e0e6761159ed872680640
KoreanGitUser <111973658+KoreanGitUser@users.noreply.github.com>
2022-12-13 08:09:24 +0900

    update if empty chatList

--- src/components/home/ChatListPage.tsx
+++ src/components/home/ChatListPage.tsx
@@ -3,7 +3,10 @@ import ChatList from "@utils/common/app/ChatList";
 const ChatListPage = () => {
   return (
     <div className="flex flex-col justify-start items-start w-full h-full max-h-[1000rem] gap-2">
-      <ChatList
+      <span className="flex justify-center items-center h-full w-full text-lg ml-1">
+        마음에 드는 짝에게 먼저 대화를 시도해보세요💕
+      </span>
+      {/* <ChatList
         img="bibi.png"
         username="비비"
         chat="비비 또는 BIBI는 대한민국의 가수, 배우이다. 2018년부터 2019년까지 방영된 《더 팬》에 참가하여 준우승을 하였다."
@@ -52,7 +55,7 @@ const ChatListPage = () => {
         img="shuhwa.jpg"
         username="비비"
         chat="비비 또는 BIBI는 대한민국의 가수, 배우"
-      />
+      /> */}
     </div>
   );
 };
